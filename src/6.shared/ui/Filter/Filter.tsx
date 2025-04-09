67428e03f37bd82484b0212ad5cfd98bbf70f165
Uttake <88742186+Uttake@users.noreply.github.com>
2025-04-09 13:12:05 +0300

    feat(Notification): Notification and Header title container

---
+++ src/6.shared/ui/Filter/Filter.tsx
@@ -0,0 +1,11 @@
+import FilterIco from '../../assets/icons/filter.svg?react'
+
+const FilterICon = () => {
+	return (
+		<div className='group cursor-pointer p-2.5 flex justify-center items-center rounded-2xl border-[1px] border-[#40434f] w-11 h-11 hover:bg-[#40434f] transition-all'>
+			<FilterIco className='fill-white' />
+		</div>
+	)
+}
+
+export default FilterICon
---
+++ src/6.shared/ui/NotificationHeader/NotificationHeader.tsx
@@ -0,0 +1,35 @@
+import BackIco from '../../assets/icons/back.svg?react'
+import NotificationIco from '../../assets/images/notification.svg?react'
+
+import styles from './NotificationHeader.module.css'
+
+export type NotificationHeaderProps = {
+	title: string
+	back?: boolean
+}
+
+export const NotificationHeader = ({
+	title,
+	back = false
+}: NotificationHeaderProps) => {
+	const handleBack = () => {
+		window.history.back()
+	}
+
+	return (
+		<div className='flex justify-between mb-4 items-center px-2.5'>
+			<div className='flex items-center gap-2.5'>
+				{back && (
+					<button className='cursor-pointer' onClick={handleBack}>
+						<BackIco />
+					</button>
+				)}
+				<h1 className={styles.title}>{title}</h1>
+			</div>
+			<button className='cursor-pointer'>
+				{' '}
+				<NotificationIco fill='white' />
+			</button>
+		</div>
+	)
+}
