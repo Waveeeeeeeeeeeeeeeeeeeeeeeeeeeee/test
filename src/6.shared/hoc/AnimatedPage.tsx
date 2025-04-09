cde2a4c7e564824c02c31764a7be048f86e84c42
Uttake <88742186+Uttake@users.noreply.github.com>
2025-04-09 13:09:05 +0300

    feat(AnimatedPage): animate component

---
+++ src/6.shared/hoc/AnimatedPage.tsx
@@ -0,0 +1,22 @@
+import { motion } from 'framer-motion'
+import { FC } from 'react'
+
+export const AnimatedPage = (Page: FC) => {
+	const AnimatedComponent: FC = () => {
+		return (
+			<motion.div
+				className='static flex flex-col flex-1'
+				initial={{ opacity: 0, scale: 0.9, y: 20 }}
+				animate={{ opacity: 1, scale: 1, y: 0 }}
+				exit={{ opacity: 0, scale: 0.9, y: -20 }}
+				transition={{
+					duration: 0.25,
+					ease: 'linear'
+				}}
+			>
+				<Page />
+			</motion.div>
+		)
+	}
+	return AnimatedComponent
+}
