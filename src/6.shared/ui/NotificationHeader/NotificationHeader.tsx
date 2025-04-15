0567a8c58b87aaaa80ec68d7cbe8a3b0aae953af
Uttake <88742186+Uttake@users.noreply.github.com>
2025-04-15 13:48:56 +0300

    feat(NotificationHeader): different use variant

--- src/6.shared/ui/NotificationHeader/NotificationHeader.tsx
+++ src/6.shared/ui/NotificationHeader/NotificationHeader.tsx
@@ -5,31 +5,33 @@ import styles from './NotificationHeader.module.css'
 
 export type NotificationHeaderProps = {
 	title: string
+	goBack: () => void
 	back?: boolean
+	notification: boolean
 }
 
 export const NotificationHeader = ({
 	title,
-	back = false
+	goBack,
+	back = false,
+	notification
 }: NotificationHeaderProps) => {
-	const handleBack = () => {
-		window.history.back()
-	}
-
 	return (
-		<div className='flex justify-between mb-4 items-center px-2.5'>
+		<div className='flex justify-between mb-4 items-center'>
 			<div className='flex items-center gap-2.5'>
 				{back && (
-					<button className='cursor-pointer' onClick={handleBack}>
+					<button className='cursor-pointer' onClick={goBack}>
 						<BackIco />
 					</button>
 				)}
 				<h1 className={styles.title}>{title}</h1>
 			</div>
-			<button className='cursor-pointer'>
-				{' '}
-				<NotificationIco fill='white' />
-			</button>
+			{notification && (
+				<button className='cursor-pointer'>
+					{' '}
+					<NotificationIco fill='white' />
+				</button>
+			)}
 		</div>
 	)
 }
