fab9a4199d66f6bf1865d6eeee805f03e9a05f83
Uttake <88742186+Uttake@users.noreply.github.com>
2025-04-15 14:42:08 +0300

    feat(Modal): Modal component/ toggle store state

---
+++ src/6.shared/ui/Modal/Modal.tsx
@@ -0,0 +1,31 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import type { PropsWithChildren } from 'react'
+
+type Props = {
+	isOpen: boolean
+}
+
+export const Modal = ({ children, isOpen }: PropsWithChildren<Props>) => {
+	return (
+		<AnimatePresence>
+			{isOpen && (
+				<motion.div
+					className='fixed inset-0 bg-black bg-opacity-50 flex justify-center w-full h-screen z-[200]'
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+				>
+					<motion.div
+						className='bg-[var(--main-bg)] w-full rounded-t-2xl p-4 overflow-y-auto'
+						initial={{ y: 100, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: 100, opacity: 0 }}
+						transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+					>
+						{children}
+					</motion.div>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	)
+}
