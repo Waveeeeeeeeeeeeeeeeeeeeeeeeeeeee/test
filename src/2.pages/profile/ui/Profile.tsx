eaac7eab6f7ff1225635941481b7a206bdf95dab
Uttake <88742186+Uttake@users.noreply.github.com>
2025-04-15 20:10:28 +0300

    feat(Profile): component skeleton, btn image

--- src/2.pages/profile/ui/Profile.tsx
+++ src/2.pages/profile/ui/Profile.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
+
 import { useUserStore } from '@/5.entities/user/model/store'
+import { useCustomTranslation } from '@/6.shared'
 import { AnimatedPage } from '@/6.shared/hoc/AnimatedPage'
+import { ToggleTabs } from '@/6.shared/ui/ToggleTabs/ToggleTabs'
 import { UserCard } from '@/6.shared/ui/UserCard/UserCard'
 
 const Profile = () => {
+	const [toggle, setToggle] = useState('description')
 	const { profile, telegram } = useUserStore()
+	const { description, games } = useCustomTranslation('profile')
 
+	const profileOptions = [
+		{
+			label: description,
+			value: 'description'
+		},
+		{
+			label: games,
+			value: 'games'
+		}
+	]
 	return (
 		<div className={'p-4'}>
 			<UserCard
@@ -15,6 +31,12 @@ const Profile = () => {
 				languages={profile.selectedLanguage}
 				avatarUrl={profile.image || telegram?.photo_url || ''}
 				icon='notification'
+			/>
+			<ToggleTabs
+				options={profileOptions}
+				active={toggle}
+				onChange={setToggle}
+				variant='base'
 			/>
 		</div>
 	)
