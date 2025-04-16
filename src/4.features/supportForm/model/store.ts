4ee0204c786bfede2b715fc193a2fa2594c694a4
Uttake <88742186+Uttake@users.noreply.github.com>
2025-04-16 16:09:31 +0300

    feat(ProfileSupport): Новый компонент обращения в поддержку со сторам и типами

---
+++ src/4.features/supportForm/model/store.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand'
+
+interface SupportFormState {
+  topic: string
+  description: string
+  image: File | null
+  setTopic: (topic: string) => void
+  setDescription: (desc: string) => void
+  setImage: (file: File | null) => void
+}
+
+export const useSupportFormStore = create<SupportFormState>((set) => ({
+  topic: '',
+  description: '',
+  image: null,
+  setTopic: (topic) => set({ topic }),
+  setDescription: (description) => set({ description }),
+  setImage: (image) => set({ image }),
+}))
