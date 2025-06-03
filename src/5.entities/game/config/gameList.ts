4567293821c17a467a32680a4a7f5b7260d4fd8d
Uttake <88742186+Uttake@users.noreply.github.com>
2025-06-03 18:59:37 +0300

    chore: GameList for new Type

--- src/5.entities/game/config/gameList.ts
+++ src/5.entities/game/config/gameList.ts
@@ -2,39 +2,45 @@ import { Game } from '../model/types';
 
 export const gameList: Game[] = [
   {
-    id: 'pubg-1',
+    id: '1',
     title: 'PUBG',
     icon: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/c9/%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%B8%D0%B3%D1%80%D1%8B_PlayerUnknown%27s_Battlegrounds.jpg/411px-%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%B8%D0%B3%D1%80%D1%8B_PlayerUnknown%27s_Battlegrounds.jpg',
     players: 1382,
+    purposes:[],
   },
   {
-    id: 'overwatch-1',
+    id: '2',
     title: 'Overwatch',
     icon: 'https://upload.wikimedia.org/wikipedia/en/5/51/Overwatch_cover_art.jpg',
     players: 1382,
+    purposes:[],
   },
   {
-    id: 'fc25-1',
+    id: '3',
     title: 'FC25',
     icon: 'https://upload.wikimedia.org/wikipedia/ru/0/09/EA_FC_25_Cover.jpg',
     players: 1382,
+    purposes:[],
   },
   {
-    id: 'dota2-1',
+    id: '4',
     title: 'Dota 2',
     icon: 'https://upload.wikimedia.org/wikipedia/ru/thumb/8/8e/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_Dota_2.jpg/400px-%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_Dota_2.jpg',
     players: 1382,
+    purposes:[],
   },
   {
-    id: 'pubg-2',
+    id: '5',
     title: 'PUBG',
     icon: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/c9/%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%B8%D0%B3%D1%80%D1%8B_PlayerUnknown%27s_Battlegrounds.jpg/411px-%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%B8%D0%B3%D1%80%D1%8B_PlayerUnknown%27s_Battlegrounds.jpg',
     players: 1382,
+    purposes:[],
   },
   {
-    id: 'pubg-3',
+    id: '6',
     title: 'PUBG',
     icon: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/c9/%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%B8%D0%B3%D1%80%D1%8B_PlayerUnknown%27s_Battlegrounds.jpg/411px-%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%B8%D0%B3%D1%80%D1%8B_PlayerUnknown%27s_Battlegrounds.jpg',
     players: 1382,
+    purposes:[],
   },
 ];
