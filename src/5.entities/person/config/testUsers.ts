c931c25301f915a9919b80b674d00bf4fd86bd65
Uttake <88742186+Uttake@users.noreply.github.com>
2025-06-10 18:21:59 +0300

    chore: mock users/friends

---
+++ src/5.entities/person/config/testUsers.ts
@@ -0,0 +1,70 @@
+import { Gender, UserProfile } from "@/5.entities/user/model/types";
+
+
+export const mockUsers: UserProfile[] = [
+  {
+    age: '22',
+    nickname: 'Uttake',
+    gender: 'Male' as Gender,
+    country: 'Russia',
+    city: 'Berlin',
+    about: 'brbrbrbrbrbrbrbrbrbrbrb',
+    interests: ['ystal'],
+    games: [],
+    image: null,
+    selectedLanguage: 'ru',
+    selectedMatchType: 'Friendship',
+    isFirstFormValid: true,
+    isSecondFormValid: true,
+      country_code: 'RU'
+  },
+   {
+    age: '22',
+    nickname: 'Uttake2',
+    gender: 'Male' as Gender,
+    country: 'Russia',
+    city: 'Berlin',
+    about: 'brbrbrbrbrbrbrbrbrbrbrb',
+    interests: ['ystal'],
+    games: [],
+    image: null,
+    selectedLanguage: 'en',
+    selectedMatchType: 'Friendship',
+    isFirstFormValid: true,
+    isSecondFormValid: true,
+    country_code: 'RU'
+
+  },
+    {
+    age: '22',
+    nickname: 'Uttake3',
+    gender: 'Male' as Gender,
+    country: 'Russia',
+    city: 'Berlin',
+    about: 'brbrbrbrbrbrbrbrbrbrbrb',
+    interests: ['ystal'],
+    games: [],
+    image: null,
+    selectedLanguage: 'ua',
+    selectedMatchType: 'Friendship',
+    isFirstFormValid: true,
+    isSecondFormValid: true,
+      country_code: 'RU'
+  },
+    {
+    age: '22',
+    nickname: 'Uttake4',
+    gender: 'Male' as Gender,
+    country: 'Russia',
+    city: 'Berlin',
+    about: 'brbrbrbrbrbrbrbrbrbrbrb',
+    interests: ['ystal'],
+    games: [],
+    image: null,
+    selectedLanguage: 'ru',
+    selectedMatchType: 'Friendship',
+    isFirstFormValid: true,
+    isSecondFormValid: true,
+    country_code: 'RU'
+  },
+]
